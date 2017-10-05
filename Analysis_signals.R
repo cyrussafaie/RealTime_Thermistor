289a8dbd203bb6c1c7d56fd4b6f10239fb61ec1f
cyrussafaie <cyrus.safaie@opexanalytics.com>
2017-10-04 23:15:01 -0500

    analysis file

--- Analysis_signals.R
+++ Analysis_signals.R
@@ -11,13 +11,21 @@ library(plyr)
 library(rollply)
 library(dtplyr)
 library(zoo)
-temp_events=temp_events %>%
-        #group_by(group) %>%
-        mutate(Diff = c(0, diff(temp_events$temp)))
-temp_events=temp_events %>%
-        mutate(mean_past10=rollapply(Diff,list(-10:0), function(a) mean(a),fill = NA))
+temp_events_org=temp_events_org[,1:2]
+names(temp_events_org)
 
-plot(temp_events$mean_past10)
+temp_events_org=temp_events_org %>%
+        #group_by(group) %>%
+        mutate(Diff = c(0, diff(temp_events_org$temp)))
+temp_events_org=temp_events_org %>%
+        mutate(mean_past10=rollapply(Diff,list(-10:0), function(a) mean(a),fill = NA)
+               ,mean_past20=rollapply(Diff,list(-20:0), function(a) mean(a),fill = NA)
+               ,mean_past40=rollapply(Diff,list(-40:0), function(a) mean(a),fill = NA))
+plot(temp_events_org$Diff)
+plot(temp_events_org$mean_past10)
+plot(temp_events_org$mean_past20)
+plot(temp_events_org$mean_past40)
+abline(h=0.002, col='red')
 hist(temp_events$Diff)
 min(temp_events$Diff)
 
